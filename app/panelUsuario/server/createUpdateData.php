22a0d1ae12b01dbd98cbab52fd0ce00ab3f83c36
SrJowy <joelbraortiz@gmail.com>
2021-11-02 22:41:40 +0100

    Release 1.0.2 - Solved bug at game modification

--- app/panelUsuario/server/createUpdateData.php
+++ app/panelUsuario/server/createUpdateData.php
@@ -32,26 +32,35 @@ if ($partidaAct == null) {
     $query = "SELECT * FROM partida WHERE nombreUsuario = '$username' AND num_partida = '$numP';";
     $res = mysqli_query($db,$query);
     $partida = mysqli_fetch_assoc($res);
-    if ($partida) {
+    if ($partida) { //Si existe partida con el código introducido --> buscar si se han hecho cambios
         $query = "SELECT * FROM partida WHERE nombreUsuario = '$username' AND num_partida = '$numP' AND mapa = '$mapa' AND bajas = '$bajas' AND puntos = '$puntos' AND muertes = '$muertes';";
         $res = mysqli_query($db, $query);
         $partida = mysqli_fetch_assoc($res);
-        if ($partida) {
+        if ($partida) { //Si no existen cambios --> error
             $_SESSION['errorPartidaExiste'] = true;
-            $_SESSION['numP'] = $numP;
+            $_SESSION['numP'] = $numP;  //Enviamos de vuelta todos los datos ya introducidos previamente
             $_SESSION['mapa'] = $mapa;
             $_SESSION['puntos'] = $puntos;
             $_SESSION['bajas'] = $bajas;
             $_SESSION['muertes'] = $muertes;
             $_SESSION['partidaAnt'] = $partidaAct;
             header('location: ../modificar.php');
-        } else {
+        } elseif ($partidaAct == $numP) { //Si se han hecho cambios --> actualizar datos partida
             $query = "UPDATE partida SET num_partida = '$numP', mapa = '$mapa', puntos='$puntos', bajas = '$bajas', muertes = '$muertes' WHERE nombreUsuario = '$username' AND num_partida = '$partidaAct';";
             unset($_SESSION['errorPartidaExiste']);
             mysqli_query($db,$query);
             header('location: ../partidasGuardadas.php');
+        } else {
+            $_SESSION['errorPartidaExiste'] = true;
+            $_SESSION['numP'] = $numP;  //Enviamos de vuelta todos los datos ya introducidos previamente
+            $_SESSION['mapa'] = $mapa;
+            $_SESSION['puntos'] = $puntos;
+            $_SESSION['bajas'] = $bajas;
+            $_SESSION['muertes'] = $muertes;
+            $_SESSION['partidaAnt'] = $partidaAct;
+            header('location: ../modificar.php');
         }
-    } else {
+    } else { //Si la partida no existe --> actualizar partida con datos nuevos (nuevo códido de partida)
         $query = "UPDATE partida SET num_partida = '$numP', mapa = '$mapa', puntos='$puntos', bajas = '$bajas', muertes = '$muertes' WHERE nombreUsuario = '$username' AND num_partida = '$partidaAct';";
         unset($_SESSION['errorPartidaExiste']);
         mysqli_query($db,$query);
