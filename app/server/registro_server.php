4055d70b030454d84c1cf0e590e228ed0f2ce9a4
Bosco <boscoarangurensaiz@gmail.com>
2021-12-09 18:50:59 +0100

    contraseña con 8 carac (num, minus, mayus, extra) solo php

--- app/server/registro_server.php
+++ app/server/registro_server.php
@@ -21,29 +21,53 @@ $nombreUsuario = $_POST['username'];
 $contra = $_POST['contra'];
 $error = false;
 
-
-$user_check_query = "SELECT * FROM usuario WHERE nombreUsuario = '$nombreUsuario';";
-$res = mysqli_query($db, $user_check_query);
-$usuarioNombre = mysqli_fetch_assoc($res);
-
-$user_check_query = "SELECT * FROM usuario WHERE email = '$email';";
-$res = mysqli_query($db, $user_check_query);
-$usuarioMail = mysqli_fetch_assoc($res);
-
-if ($usuarioMail || $usuarioNombre) {
-    $error = true;
-    if ($usuarioMail) $_SESSION['errorMail'] = $email;
-    if ($usuarioNombre) $_SESSION['errorUsername'] = $nombreUsuario;
+$contra_split = str_split($contra);
+$mayus = False;
+$minus = False;
+$num = False;
+$extra = False;
+$errorContra = False;
+foreach ($contra_split as &$char) {
+    if (48<=ord($char)<=57){
+        $num = True;
+    } elseif (65<=ord($char)<=90) {
+        $mayus = True; 
+    } elseif (97<=ord($char)<=122) {
+        $minus = True;
+    } elseif (33<=ord($char)<=47 || 58<=ord($char)<=64 || 91<=ord($char)<=96 || 123<=ord($char)<=126) {
+        $extra = True;
+    } else {
+        $errorContra = True;
+    }
 }
 
-if (!$error){
-    $query = "INSERT INTO usuario VALUES ('$nombre', '$apellidos', '$dni', '$tel', '$fecha', '$email', '$contra', '$nombreUsuario');";    
-    $res = mysqli_query($db, $query);
-    header('location: ../index.php');
-} else {    
-    header('location: ../registro.php');
-}
+if (strlen($contra)<8) {
+    //error la contraseña debe tener al menos 8 caracteres
+} elseif ( !$num || !$mayus || !$minus || !$extra || $errorContra ){
+    //error la contraseña debe de tener al menos un número, una minuscula, una mayúscula y un caracter no alfanúmerico
+} else {
+    $user_check_query = "SELECT * FROM usuario WHERE nombreUsuario = '$nombreUsuario';";
+    $res = mysqli_query($db, $user_check_query);
+    $usuarioNombre = mysqli_fetch_assoc($res);
 
+    $user_check_query = "SELECT * FROM usuario WHERE email = '$email';";
+    $res = mysqli_query($db, $user_check_query);
+    $usuarioMail = mysqli_fetch_assoc($res);
+
+    if ($usuarioMail || $usuarioNombre) {
+        $error = true;
+        if ($usuarioMail) $_SESSION['errorMail'] = $email;
+        if ($usuarioNombre) $_SESSION['errorUsername'] = $nombreUsuario;
+    }
+
+    if (!$error){
+        $query = "INSERT INTO usuario VALUES ('$nombre', '$apellidos', '$dni', '$tel', '$fecha', '$email', '$contra', '$nombreUsuario');";    
+        $res = mysqli_query($db, $query);
+        header('location: ../index.php');
+    } else {    
+        header('location: ../registro.php');
+    }
+}
 
 ?>
 
